c7d0bb6eb4d4377f4ce2d7c8663379ca58521210
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-11-14 04:37:58 +0100

    Added a test

---
+++ test/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Change this line to be able to call it from elswhere
+#e.g. TESTS_DIR=~/Code/HuffmanCoding/test
+TESTS_DIR=$PWD
+
+# Dependencies
+TESTFILE="$TESTS_DIR/humanrights.txt"
+ENCODED="$TESTS_DIR/humanrights.huf"
+NEWENCODED="$TESTS_DIR/humanrights_copy.huf"
+DECODED="$TESTS_DIR/humanrights_copy.txt"
+
+# Running program
+cd ..
+./huffcode -e "$TESTFILE"
+mv "$ENCODED" "$NEWENCODED"
+./huffcode -d "$NEWENCODED"
+
+# Showing diffs
+echo
+echo "Difference between original and encoded-decoded (blank if none, ok):"
+diff "$TESTFILE" "$DECODED"
+echo
+
+# Showing compression
+OWEIGHT=$(wc -c "$TESTFILE" | awk '{print $1}')
+CWEIGHT=$(wc -c "$NEWENCODED" | awk '{print $1}')
+
+echo "The original weights:   $OWEIGHT bytes"
+echo "The compressed weights:  $CWEIGHT bytes"
+
+echo "This represents a $(( (( (($OWEIGHT-$CWEIGHT)) * 100))/$OWEIGHT))% reduction"
+
+# Removing test files
+rm -f "$NEWENCODED"
+rm -f "$DECODED"
